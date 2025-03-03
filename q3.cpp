fb0f6301302ffbe63701ee6a050a1a2a5546b3a5
kaonashie <chenardkone@gmail.com>
2025-03-03 00:28:44 -0500

    q3

--- q3.cpp
+++ q3.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+int digitalRoot(int n);
+
 int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    std::cout << "The digital root of " << num << " is " << digitalRoot(num) << std::endl;
     return 0;
+}
+
+int digitalRoot(int n) {
+    if (n < 10) {
+        return n;
+    }
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return digitalRoot(sum);
 }
