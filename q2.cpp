685a9af6dd9bc2a4b3a8dd3604db2997a2cf148f
nirmalnpatel111 <140967348+nirmalnpatel111@users.noreply.github.com>
2025-03-06 19:51:56 -0500

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
+bool testPalindromeHelper(const string &str, int start, int end)
+{
+    if (start >= end) {
+        return true;
+    }
+
+    if (str[start] != str[end]) {
+        return false;
+    }
+
+    return testPalindromeHelper(str, start + 1, end - 1);
+}
+
+bool testPalindrome(const string &str)
+{
+    return testPalindromeHelper(str, 0, str.size() - 1);
+}
+
+int main()
+{
+    cout << "Enter a string: ";
+    string input;
+    getline(cin, input);
+
+    if (testPalindrome(input)) {
+        cout << "\"" << input << "\" is a palindrome.\n";
+    } else {
+        cout << "\"" << input << "\" is NOT a palindrome.\n";
+    }
+
     return 0;
 }
